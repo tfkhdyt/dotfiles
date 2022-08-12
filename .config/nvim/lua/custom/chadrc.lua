b6281de13e6a01ab845baabcb9d5b4a693f2609e
tfkhdyt <tfkhdyt@pm.me>
2022-08-12 11:19:44 +0700

    🐛 fix(nvim): move custom settings to custom folder and clean up custom/init.lua

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -6,22 +6,22 @@ local M = {}
 -- example of changing theme:
 
 M.ui = {
-  theme = "doomchad",
+	theme = "doomchad",
 }
 
 M.plugins = {
-  user = {
-    ["goolord/alpha-nvim"] = {
-      disable = false,
-    },
-    ["jose-elias-alvarez/null-ls.nvim"] = {
-      after = "nvim-lspconfig",
-      config = function()
-        require "custom.plugins.null-ls"
-      end,
-    },
-    ["windwp/nvim-ts-autotag"] = {},
-  },
+	user = {
+		["goolord/alpha-nvim"] = {
+			disable = false,
+		},
+		["jose-elias-alvarez/null-ls.nvim"] = {
+			after = "nvim-lspconfig",
+			config = function()
+				require("custom.plugins.null-ls")
+			end,
+		},
+		["windwp/nvim-ts-autotag"] = {},
+	},
 }
 
 return M
--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -1,45 +1,10 @@
 -- example file i.e lua/custom/init.lua
 
 -- load your globals, autocmds here or anything .__.
+local opt = vim.opt
 
-local present, null_ls = pcall(require, "null-ls")
+-- custom options
+opt.breakindent = true
+opt.showbreak = "  "
+opt.relativenumber = true
 
-if not present then
-   return
-end
-
-local b = null_ls.builtins
-
-local sources = {
-
-   -- webdev stuff
-   b.formatting.deno_fmt,
-   b.formatting.prettier,
-
-   -- Lua
-   b.formatting.stylua,
-
-   -- Shell
-   b.formatting.shfmt,
-   b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
-
-   -- eslint
-   b.code_actions.eslint,
-   b.diagnostics.eslint,
-   b.diagnostics.tsc,
-   b.formatting.eslint,
-   b.formatting.prettier,
-
-   -- go
-   b.diagnostics.golangci_lint,
-   b.formatting.gofmt,
-   b.formatting.goimports,
-   b.code_actions.refactoring
-}
-
-null_ls.setup {
-   debug = true,
-   sources = sources,
-}
-
--- load it after nvim-lspconfig cuz we lazy loaded lspconfig
