4580a05ece1f85bb512b65207a32f50f02e40a42
tfkhdyt <tfkhdyt@pm.me>
2022-10-06 14:30:39 +0700

    feat(mpris module): add ellipsis

--- .config/polybar/player-mpris-simple.sh
+++ .config/polybar/player-mpris-simple.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
 player_status=$(playerctl status 2> /dev/null)
+L=16
 
 if [ "$player_status" = "Playing" ]; then
-    echo " $(playerctl metadata artist) - $(playerctl metadata title)"
+    title=$(playerctl metadata title)
+    if [[ ${#title} -gt $L ]]; then
+      title="${title:0:L}..."
+    else
+      title="$title"
+    fi
+    echo " $(playerctl metadata artist) - $title"
 elif [ "$player_status" = "Paused" ]; then
-    echo " $(playerctl metadata artist) - $(playerctl metadata title)"
-else
-    echo ""
+    title=$(playerctl metadata title)
+    if [[ ${#title} -gt $L ]]; then
+      title="${title:0:L}..."
+    else
+      title="$title"
+    fi
+    echo " $(playerctl metadata artist) - $title"
+# else
+#     echo ""
 fi
