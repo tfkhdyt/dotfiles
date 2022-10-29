096ca23e0e739e2a61aea2b4d9f93bb72e06aa6c
tfkhdyt <tfkhdyt@pm.me>
2022-10-29 19:08:42 +0700

    feat(i3): switch to xautolock

--- .config/i3/locker.sh
+++ .config/i3/locker.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
 # Only exported variables can be used within the timer's command.
-export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
+# export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
 
-# Run xidlehook
-xidlehook \
-  `# Don't lock when there's a fullscreen application` \
-  --not-when-fullscreen \
-  `# Don't lock when there's audio playing` \
-  --not-when-audio \
-  `# Dim the screen after 60 seconds, undim if user becomes active` \
-  --timer 600 \
-    'xrandr --output "$PRIMARY_DISPLAY" --brightness .1' \
-    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
-  `# Undim & lock after 10 more seconds` \
-  --timer 300 \
-    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1; i3lock-fancy -- maim' \
-    '' \
-  `# Finally, suspend an hour after it locks` \
-  --timer 3600 \
-    'systemctl suspend' \
-    ''
+xautolock -time 10 -locker "i3lock-fancy -- maim" -detectsleep -corners "00--" -killtime 30 -killer "systemctl suspend"
+
+# # Run xidlehook
+#xidlehook \
+#   `# Don't lock when there's a fullscreen application` \
+#   --not-when-fullscreen \
+#   `# Don't lock when there's audio playing` \
+#   --not-when-audio \
+#   `# Dim the screen after 60 seconds, undim if user becomes active` \
+#   --timer 600 \
+#     'xrandr --output "$PRIMARY_DISPLAY" --brightness .1' \
+#     'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
+#   `# Undim & lock after 10 more seconds` \
+#   --timer 300 \
+#     'xrandr --output "$PRIMARY_DISPLAY" --brightness 1; i3lock-fancy -- maim' \
+#     '' \
+#   `# Finally, suspend an hour after it locks` \
+   # --timer 1800 \
+   #   'systemctl suspend' \
+   #   ''
 
