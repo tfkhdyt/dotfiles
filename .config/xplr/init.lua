88143537058b9646039b4f1c1dccfabf82eb0e16
tfkhdyt <tfkhdyt@pm.me>
2022-11-04 16:44:33 +0700

    fix(xplr): change file and directory icon when in rename mode

--- .config/xplr/init.lua
+++ .config/xplr/init.lua
@@ -122,3 +122,90 @@ xplr.config.modes.builtin.go_to = {
   },
 }
 
+
+-- The builtin create directory mode.
+--
+-- Type: [Mode](https://xplr.dev/en/mode)
+xplr.config.modes.builtin.create_directory = {
+  name = "create directory",
+  prompt = " ❯ ",
+  key_bindings = {
+    on_key = {
+      ["tab"] = {
+        help = "try complete",
+        messages = {
+          { CallLuaSilently = "builtin.try_complete_path" },
+        },
+      },
+      ["enter"] = {
+        help = "submit",
+        messages = {
+          {
+            BashExecSilently0 = [===[
+              PTH="$XPLR_INPUT_BUFFER"
+              PTH_ESC=$(printf %q "$PTH")
+              if [ "$PTH" ]; then
+                mkdir -p -- "$PTH" \
+                && "$XPLR" -m 'SetInputBuffer: ""' \
+                && "$XPLR" -m ExplorePwd \
+                && "$XPLR" -m 'LogSuccess: %q' "$PTH_ESC created" \
+                && "$XPLR" -m 'FocusPath: %q' "$PTH"
+              else
+                "$XPLR" -m PopMode
+              fi
+            ]===],
+          },
+        },
+      },
+    },
+    default = {
+      messages = {
+        "UpdateInputBufferFromKey",
+      },
+    },
+  },
+}
+
+-- The builtin create file mode.
+--
+-- Type: [Mode](https://xplr.dev/en/mode)
+xplr.config.modes.builtin.create_file = {
+  name = "create file",
+  prompt = " ❯ ",
+  key_bindings = {
+    on_key = {
+      ["tab"] = {
+        help = "try complete",
+        messages = {
+          { CallLuaSilently = "builtin.try_complete_path" },
+        },
+      },
+      ["enter"] = {
+        help = "submit",
+        messages = {
+          {
+            BashExecSilently0 = [===[
+              PTH="$XPLR_INPUT_BUFFER"
+              PTH_ESC=$(printf %q "$PTH")
+              if [ "$PTH" ]; then
+                mkdir -p -- "$(dirname $PTH)" \
+                && touch -- "$PTH" \
+                && "$XPLR" -m 'SetInputBuffer: ""' \
+                && "$XPLR" -m 'LogSuccess: %q' "$PTH_ESC created" \
+                && "$XPLR" -m 'ExplorePwd' \
+                && "$XPLR" -m 'FocusPath: %q' "$PTH"
+              else
+                "$XPLR" -m PopMode
+              fi
+            ]===],
+          },
+        },
+      },
+    },
+    default = {
+      messages = {
+        "UpdateInputBufferFromKey",
+      },
+    },
+  },
+}
