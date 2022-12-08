8c7670ac06fdedbe4cbb80771af06bfb2e31cec0
tfkhdyt <tfkhdyt@proton.me>
2022-12-08 20:50:15 +0700

    fix(xplr): comment out zsh setting

--- .config/xplr/init.lua
+++ .config/xplr/init.lua
@@ -76,11 +76,11 @@ key.v = {
 key["f3"] = xplr.config.modes.custom.nuke.key_bindings.on_key.v
 key["enter"] = xplr.config.modes.custom.nuke.key_bindings.on_key.o
 
-xplr.config.modes.builtin.action.key_bindings.on_key["!"].messages = {
-  { Call = { command = "zsh", args = { "-i" } } },
-  "ExplorePwdAsync",
-  "PopMode",
-}
+-- xplr.config.modes.builtin.action.key_bindings.on_key["!"].messages = {
+--   { Call = { command = "zsh", args = { "-i" } } },
+--   "ExplorePwdAsync",
+--   "PopMode",
+-- }
 
 local colors = {
   blue = { Rgb = { 138, 173, 244 } },
@@ -113,71 +113,71 @@ xplr.config.node_types.mime_essence = {
 
 }
 
-xplr.config.modes.builtin.go_to = {
-  name = "go to",
-  key_bindings = {
-    on_key = {
-      ["f"] = {
-        help = "follow symlink",
-        messages = {
-          "FollowSymlink",
-          "PopMode",
-        },
-      },
-      ["g"] = {
-        help = "top",
-        messages = {
-          "FocusFirst",
-          "PopMode",
-        },
-      },
-      ["p"] = {
-        help = "path",
-        messages = {
-          "PopMode",
-          { SwitchModeBuiltin = "go_to_path" },
-          { SetInputBuffer = "" },
-        },
-      },
-      ["i"] = {
-        help = "initial $PWD",
-        messages = {
-          "PopMode",
-          {
-            BashExecSilently0 = [===[
-              "$XPLR" -m 'ChangeDirectory: %q' "${XPLR_INITIAL_PWD:?}"
-            ]===],
-          },
-        },
-      },
-      ["x"] = {
-        help = "open in gui",
-        messages = {
-          {
-            BashExecSilently0 = [===[
-              if [ -z "$OPENER" ]; then
-                if command -v xdg-open; then
-                  OPENER="xdg-open"
-                elif command -v open; then
-                  OPENER=open
-                else
-                  "$XPLR" -m 'LogError: "$OPENER not found"'
-                  exit 1
-                fi
-              fi
-              (while IFS= read -r -d '' PTH; do
-                $OPENER "${PTH:?}" > /dev/null 2>&1 &
-                disown
-              done < "${XPLR_PIPE_RESULT_OUT:?}")
-            ]===],
-          },
-          "ClearScreen",
-          "PopMode",
-        },
-      },
-    },
-  },
-}
+-- xplr.config.modes.builtin.go_to = {
+--   name = "go to",
+--   key_bindings = {
+--     on_key = {
+--       ["f"] = {
+--         help = "follow symlink",
+--         messages = {
+--           "FollowSymlink",
+--           "PopMode",
+--         },
+--       },
+--       ["g"] = {
+--         help = "top",
+--         messages = {
+--           "FocusFirst",
+--           "PopMode",
+--         },
+--       },
+--       ["p"] = {
+--         help = "path",
+--         messages = {
+--           "PopMode",
+--           { SwitchModeBuiltin = "go_to_path" },
+--           { SetInputBuffer = "" },
+--         },
+--       },
+--       ["i"] = {
+--         help = "initial $PWD",
+--         messages = {
+--           "PopMode",
+--           {
+--             BashExecSilently0 = [===[
+--               "$XPLR" -m 'ChangeDirectory: %q' "${XPLR_INITIAL_PWD:?}"
+--             ]===],
+--           },
+--         },
+--       },
+--       ["x"] = {
+--         help = "open in gui",
+--         messages = {
+--           {
+--             BashExecSilently0 = [===[
+--               if [ -z "$OPENER" ]; then
+--                 if command -v xdg-open; then
+--                   OPENER="xdg-open"
+--                 elif command -v open; then
+--                   OPENER=open
+--                 else
+--                   "$XPLR" -m 'LogError: "$OPENER not found"'
+--                   exit 1
+--                 fi
+--               fi
+--               (while IFS= read -r -d '' PTH; do
+--                 $OPENER "${PTH:?}" > /dev/null 2>&1 &
+--                 disown
+--               done < "${XPLR_PIPE_RESULT_OUT:?}")
+--             ]===],
+--           },
+--           "ClearScreen",
+--           "PopMode",
+--         },
+--       },
+--     },
+--   },
+-- }
 
 
 -- The builtin create directory mode.
