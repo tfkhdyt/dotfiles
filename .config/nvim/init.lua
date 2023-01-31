21f36263d80b2c3e59e2bb2e97e5262bd257269e
tfkhdyt <tfkhdyt@proton.me>
2023-01-31 15:36:03 +0700

    fix(nvim): add break indent

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.cmd.colorscheme("catppuccin")
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.showbreak = "  "
+vim.opt.linebreak = true
