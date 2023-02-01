03db146c9460accbbc5a9b8d7e9687f8ea09ec9a
tfkhdyt <tfkhdyt@proton.me>
2023-02-02 06:31:02 +0700

    fix(polybar/launch.sh): change redirecting method

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,6 +8,6 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log 
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bar1 >> /tmp/polybar1.log 2>&1 & disown
 
 echo "Bars launched..."
