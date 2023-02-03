bea5fa7499827f43b7470181090737760143670e
tfkhdyt <tfkhdyt@proton.me>
2023-02-03 17:12:37 +0700

    feat(local): add dwm autostart

---
+++ .local/share/dwm/autostart.sh
@@ -0,0 +1,9 @@
+feh --no-fehbg --bg-fill --randomize --recursive /home/tfkhdyt/Pictures/Backgrounds/Unsplash/ &
+
+pidof picom || picom -b &
+
+pidof redshift || redshift -l -6.914744:107.609810 -t 6500:3000 &
+
+pidof slstatus || slstatus &
+
+pkill sxhkd; sxhkd &
