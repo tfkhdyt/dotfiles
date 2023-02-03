dcdef9691bd81c43310a202cdc517fca0eef9872
tfkhdyt <tfkhdyt@proton.me>
2023-02-03 17:39:32 +0700

    feat(dwm autostart): add xidlehook

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -7,3 +7,5 @@ pidof redshift || redshift -l -6.914744:107.609810 -t 6500:3000 &
 pidof slstatus || slstatus &
 
 pkill sxhkd; sxhkd &
+
+pidof xidlehook || xidlehook --detect-sleep --not-when-fullscreen --not-when-audio --timer 1800 'systemctl suspend' ''
