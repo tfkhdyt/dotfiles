b19b35590a67b91a1d40ff4825e3ac71f732cadc
tfkhdyt <tfkhdyt@proton.me>
2023-02-03 19:26:52 +0700

    feat(dwm autostart): add xss-lock

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -9,3 +9,5 @@ pidof slstatus || slstatus &
 pkill sxhkd; sxhkd &
 
 pidof xidlehook || xidlehook --detect-sleep --not-when-fullscreen --not-when-audio --timer 1800 'systemctl suspend' ''
+
+pidof xss-lock || xss-lock --transfer-sleep-lock -- slock &
