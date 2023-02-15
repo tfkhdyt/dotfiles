515d937a3832234f9237f9ceaa3ca43a0e8439a2
tfkhdyt <tfkhdyt@proton.me>
2023-02-16 06:34:51 +0700

    feat(fish): remove hydro, switch to native prompt and set git prompt and status

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -86,6 +86,35 @@ abbr -a nts nvim ~/notes/
 abbr -a kdc kdeconnect-cli
 abbr -a sudo doas
 
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
+# function fish_prompt
+#   set last_status $status
+# 
+#   set_color $fish_color_cwd
+#   printf '%s' (prompt_pwd)
+#   set_color normal
+# 
+#   printf '%s ' (__fish_git_prompt)
+# 
+#   set_color normal
+# end
+
 zoxide init fish | source
 
 if status is-interactive
