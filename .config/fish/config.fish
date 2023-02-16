27b80118124941c46b23b5ba718923d49f665c3a
tfkhdyt <tfkhdyt@proton.me>
2023-02-16 07:35:02 +0700

    fix(fish): change git status char

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -97,12 +97,13 @@ set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_dirtystate '✚'
+set __fish_git_prompt_char_stagedstate '●'
 set __fish_git_prompt_char_untrackedfiles '☡'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '+'
-set __fish_git_prompt_char_upstream_behind '-'
+set __fish_git_prompt_char_stashstate '⚑'
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
+set __fish_git_prompt_char_upstream_equal ''
 
 # function fish_prompt
 #   set last_status $status
