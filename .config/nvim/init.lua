f3006a03deca0c7e351e92bcd532a3bfb141acb3
tfkhdyt <tfkhdyt@proton.me>
2023-02-24 14:04:00 +0700

    feat(nvim plugins): add rust-tools

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,5 +10,21 @@ vim.opt.linebreak = true
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.offsetEncoding = "utf-8"
 require("lspconfig").clangd.setup({
-  capabilities = capabilities,
+	capabilities = capabilities,
 })
+
+-- rust tools
+local rt = require("rust-tools")
+
+rt.setup({
+	server = {
+		on_attach = function(_, bufnr)
+			-- Hover actions
+			vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+			-- Code action groups
+			vim.keymap.set("n", "<Leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
+		end,
+	},
+})
+
+rt.inlay_hints.enable()
