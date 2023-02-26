f7e72200f5df2bee197c6025fa69b18646a2460d
tfkhdyt <tfkhdyt@proton.me>
2023-02-26 15:33:31 +0700

    feat(nvim): setup all lsp manually

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,6 +7,15 @@ vim.opt.showbreak = "  "
 vim.opt.linebreak = true
 
 -- LSP
+require("lspconfig").bashls.setup({})
+require("lspconfig").gopls.setup({})
+require("lspconfig").rust_analyzer.setup({})
+require("lspconfig").taplo.setup({})
+require("lspconfig").tsserver.setup({})
+require("lspconfig").cssls.setup({})
+require("lspconfig").html.setup({})
+require("lspconfig").yamlls.setup({})
+
 -- local capabilities = vim.lsp.protocol.make_client_capabilities()
 -- capabilities.offsetEncoding = "utf-8"
 -- require("lspconfig").clangd.setup({
@@ -14,17 +23,17 @@ vim.opt.linebreak = true
 -- })
 
 -- rust tools
-local rt = require("rust-tools")
-
-rt.setup({
-	server = {
-		on_attach = function(_, bufnr)
-			-- Hover actions
-			vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
-			-- Code action groups
-			vim.keymap.set("n", "<Leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
-		end,
-	},
-})
-
-rt.inlay_hints.enable()
+-- local rt = require("rust-tools")
+--
+-- rt.setup({
+-- 	server = {
+-- 		on_attach = function(_, bufnr)
+-- 			-- Hover actions
+-- 			vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+-- 			-- Code action groups
+-- 			vim.keymap.set("n", "<Leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
+-- 		end,
+-- 	},
+-- })
+--
+-- rt.inlay_hints.enable()
