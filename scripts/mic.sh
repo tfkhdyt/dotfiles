2d7b83e5c54b37a2f38780085e95e7d45ad841a4
tfkhdyt <tfkhdyt@proton.me>
2023-02-27 09:20:00 +0700

    perf(mic.sh): migrate to wpctl

--- scripts/mic.sh
+++ scripts/mic.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env bash
 
-get_mic_state() {
-  pactl get-source-mute @DEFAULT_SOURCE@ | cut -d ' ' -f 2
-}
-
 toggle_mic_state() {
-  pactl set-source-mute @DEFAULT_SOURCE@ toggle
+  wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
 }
 
 send_notification() {
   dunstify -i audio-input-microphone -r 6666 "$1"
 }
 
-MUTE_STATE=$(get_mic_state)
-
-if [ "$MUTE_STATE" = "yes" ]; then
+MUTE=$(wpctl get-volume @DEFAULT_AUDIO_SOURCE@)
+if [ "$MUTE" != "${MUTE%\[MUTED\]}" ]; then
   toggle_mic_state
   send_notification "Mic status [Active]"
-elif [ "$MUTE_STATE" = "no" ]; then
+else
   toggle_mic_state
   send_notification "Mic status [Muted]"
-else
-  echo 'idk'
 fi
+
