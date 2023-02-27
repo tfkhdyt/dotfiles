e1bd78edbbc3e652f9f43821fc8d32102f132db2
tfkhdyt <tfkhdyt@proton.me>
2023-02-27 19:13:30 +0700

    fix(fish): disable up arrow for atuin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -89,7 +89,7 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
   starship init fish | source
   zoxide init fish | source
-  atuin init fish | source
+  atuin init fish --disable-up-arrow | source
 
   # Fish git prompt
   set __fish_git_prompt_showdirtystate 'yes'
