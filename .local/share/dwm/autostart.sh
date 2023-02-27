0e1c035ab2804c21313b807c038b4418e2e8cb65
tfkhdyt <tfkhdyt@proton.me>
2023-02-28 05:41:42 +0700

    fix(dwm autostart): move dwmblocks to the bottom

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -2,8 +2,6 @@ feh --no-fehbg --bg-fill --randomize --recursive /home/tfkhdyt/Pictures/Backgrou
 
 pkill sxhkd; sxhkd &
 
-pidof dwmblocks || dwmblocks &> /dev/null &
-
 pidof xss-lock || xss-lock --transfer-sleep-lock -- slock &
 
 pidof xidlehook || xidlehook --detect-sleep --not-when-fullscreen --not-when-audio --timer 1800 'systemctl suspend' '' &
@@ -12,3 +10,4 @@ pidof redshift || redshift -l -6.914744:107.609810 -t 6500:3000 &> /dev/null &
 
 pidof picom || picom -b &
 
+pidof dwmblocks || dwmblocks &> /dev/null &
