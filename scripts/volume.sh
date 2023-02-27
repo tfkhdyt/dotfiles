f032c7c6cf0b0903c225c764794761e39965c8ea
tfkhdyt <tfkhdyt@proton.me>
2023-02-27 09:20:16 +0700

    perf(volume.sh): migrate to wpctl

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,45 +1,38 @@
 #!/usr/bin/env bash
 
 # Dependencies:
-# - alsa-utils 
-# - dunst 
-
-get_volume() {
-  amixer -D pulse get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1
-}
-
-is_mute() {
-  amixer -D pulse get Master | grep '%' | grep -oE '[^ ]+$' | grep off > /dev/null
-}
+# - alsa-utils
+# - dunst
+# - wireplumber
 
 send_notification() {
   iconSound="audio-volume-high"
   iconMuted="audio-volume-muted"
-  if is_mute ; then
-    dunstify -i $iconSound -r 2593 -u normal -h int:value:0 "Volume [Muted]"
-  else
-    volume=$(get_volume)
-    level=$(echo "int:value:$volume")
-    dunstify -i $iconSound -r 2593 -u normal -h $level "Volume [${volume}%]"
-  fi
+  VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
+
+  [ "$VOL" != "${VOL%\[MUTED\]}" ] && dunstify -i $iconMuted -r 2593 -u normal -h int:value:0 "Volume [Muted]" && exit
+
+  volume=$(echo "$VOL" | awk '{printf "%.0f", $2*100}')
+  level="int:value:$volume"
+  dunstify -i $iconSound -r 2593 -u normal -h "$level" "Volume [${volume}%]"
 }
 
 case $1 in
   up)
     amixer -D pulse set Master on > /dev/null
-    pactl set-sink-volume @DEFAULT_SINK@ +5% > /dev/null
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ > /dev/null
     send_notification
-    kill -35 $(pidof dwmblocks)
+    kill -35 "$(pidof dwmblocks)"
     ;;
   down)
     amixer -D pulse set Master on > /dev/null
-    pactl set-sink-volume @DEFAULT_SINK@ -5% > /dev/null
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- > /dev/null
     send_notification
-    kill -35 $(pidof dwmblocks)
+    kill -35 "$(pidof dwmblocks)"
     ;;
   mute)
-    pactl set-sink-mute @DEFAULT_SINK@ toggle > /dev/null
+    wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle > /dev/null
     send_notification
-    kill -35 $(pidof dwmblocks)
+    kill -35 "$(pidof dwmblocks)"
     ;;
 esac
