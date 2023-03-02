ec5a92f8f0436c38f6b83ac8fec2a3467cb2d0c8
tfkhdyt <tfkhdyt@proton.me>
2023-03-02 19:39:59 +0700

    feat(null-ls): add golines and goimports_reviser

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -37,7 +37,9 @@ return {
 					-- Go
 					-- nls.builtins.diagnostics.golangci_lint,
 					nls.builtins.formatting.gofumpt,
-					nls.builtins.formatting.goimports,
+					-- nls.builtins.formatting.goimports,
+					nls.builtins.formatting.goimports_reviser,
+					nls.builtins.formatting.golines,
 				},
 			}
 		end,
