e36b36f28051be40c6459c2a1c6cfaaea3f936ae
tfkhdyt <tfkhdyt@proton.me>
2023-03-21 11:20:14 +0700

    feat(codeium): reenable codeium

--- .config/nvim/lua/plugins/codeium.lua
+++ .config/nvim/lua/plugins/codeium.lua
@@ -5,15 +5,15 @@ return {
 		vim.keymap.set("i", "<C-g>", function()
 			return vim.fn["codeium#Accept"]()
 		end, { expr = true })
-		vim.keymap.set("i", "<C-;>", function()
-			return vim.fn["codeium#CycleCompletions"](1)
-		end, { expr = true })
-		vim.keymap.set("i", "<C-,>", function()
-			return vim.fn["codeium#CycleCompletions"](-1)
-		end, { expr = true })
-		vim.keymap.set("i", "<C-x>", function()
-			return vim.fn["codeium#Clear"]()
-		end, { expr = true })
+		-- vim.keymap.set("i", "<C-]>", function()
+		-- 	return vim.fn["codeium#CycleCompletions"](1)
+		-- end, { expr = true })
+		-- vim.keymap.set("i", "<C-[>", function()
+		-- 	return vim.fn["codeium#CycleCompletions"](-1)
+		-- end, { expr = true })
+		-- vim.keymap.set("i", "<C-x>", function()
+		-- 	return vim.fn["codeium#Clear"]()
+		-- end, { expr = true })
 	end,
-	enabled = false,
+	enabled = true,
 }
