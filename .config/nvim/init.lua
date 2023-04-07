03ca555d65c75233d6ffff3fbc17c4fdbdd0d042
tfkhdyt <tfkhdyt@proton.me>
2023-04-07 09:12:33 +0700

    fix(nvim): re-enable word wrap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 -- vim.cmd.colorscheme("catppuccin")
--- vim.opt.wrap = true
--- vim.opt.breakindent = true
--- vim.opt.showbreak = "  "
--- vim.opt.linebreak = true
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.showbreak = "  "
+vim.opt.linebreak = true
 
 -- local capabilities = vim.lsp.protocol.make_client_capabilities()
 -- capabilities.offsetEncoding = "utf-8"
