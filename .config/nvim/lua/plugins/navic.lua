2e4286bb19c0c27aed085e5295d5cc3896e9cfae
tfkhdyt <tfkhdyt@proton.me>
2023-05-12 21:16:10 +0700

    fix(nvim): disable highlight in navic

---
+++ .config/nvim/lua/plugins/navic.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"SmiteshP/nvim-navic",
+		lazy = true,
+		init = function()
+			vim.g.navic_silence = true
+			require("lazyvim.util").on_attach(function(client, buffer)
+				if client.server_capabilities.documentSymbolProvider then
+					require("nvim-navic").attach(client, buffer)
+				end
+			end)
+		end,
+		opts = function()
+			return {
+				separator = " > ",
+				highlight = false,
+				depth_limit = 5,
+				icons = require("lazyvim.config").icons.kinds,
+			}
+		end,
+	},
+}
