67a7babae08e8bf9725e8a537989a00681af7276
tfkhdyt <tfkhdyt@proton.me>
2023-08-20 16:03:43 +0700

    feat(lsp): add gopls override config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,7 +6,51 @@ return {
 			diagnostics = {
 				update_in_insert = false,
 			},
-			-- autoformat = false,
+			servers = {
+				gopls = {
+					keys = {
+						-- Workaround for the lack of a DAP strategy in neotest-go: https://github.com/nvim-neotest/neotest-go/issues/12
+						{ "<leader>td", "<cmd>lua require('dap-go').debug_test()<CR>", desc = "Debug Nearest (Go)" },
+					},
+					settings = {
+						gopls = {
+							gofumpt = true,
+							codelenses = {
+								gc_details = false,
+								generate = true,
+								regenerate_cgo = true,
+								run_govulncheck = true,
+								test = true,
+								tidy = true,
+								upgrade_dependency = true,
+								vendor = true,
+							},
+							hints = {
+								assignVariableTypes = true,
+								compositeLiteralFields = true,
+								compositeLiteralTypes = true,
+								constantValues = true,
+								functionTypeParameters = true,
+								parameterNames = true,
+								rangeVariableTypes = true,
+							},
+							analyses = {
+								fieldalignment = false,
+								nilness = true,
+								unusedparams = true,
+								unusedvariable = true,
+								unusedwrite = true,
+								useany = true,
+							},
+							usePlaceholders = true,
+							-- completeUnimported = true,
+							staticcheck = true,
+							directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+							semanticTokens = true,
+						},
+					},
+				},
+			},
 		},
 	},
 }
