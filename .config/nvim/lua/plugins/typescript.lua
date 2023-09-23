0c9ad7ae7719c64866d684dbb914e453d8e45e32
tfkhdyt <me@tfkhdyt.my.id>
2023-09-23 19:38:26 +0700

    feat(nvim/typescript): enable inlay hints

--- .config/nvim/lua/plugins/typescript.lua
+++ .config/nvim/lua/plugins/typescript.lua
@@ -1,5 +1,16 @@
 return {
-	"pmizio/typescript-tools.nvim",
-	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-	opts = {},
+  "pmizio/typescript-tools.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  opts = {
+    settings = {
+      tsserver_file_preferences = {
+        includeInlayParameterNameHints = "literals",
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        quotePreference = "single",
+      },
+    },
+  },
 }
