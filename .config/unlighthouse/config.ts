655d16f13fda9bbc854bcdbf0ac48350a11b8116
tfkhdyt <me@tfkhdyt.my.id>
2023-12-08 05:09:59 +0700

    feat(unlighthouse): set sample scanner to 3

--- .config/unlighthouse/config.ts
+++ .config/unlighthouse/config.ts
@@ -1,6 +1,10 @@
 export default {
   puppeteerClusterOptions: {
-      // only run 1 worker at a time 
-    maxConcurrency: 1
-  }
-}
+    // only run 1 worker at a time
+    maxConcurrency: 1,
+  },
+  scanner: {
+    // scan each URL 3 times and average the results
+    samples: 3,
+  },
+};
