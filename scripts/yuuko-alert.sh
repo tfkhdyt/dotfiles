493b0d94b3c529c021df6029ef8ad87b0baefdbb
tfkhdyt <me@tfkhdyt.my.id>
2024-01-01 10:59:12 +0700

    feat(scripts): add yuuko alert

---
+++ scripts/yuuko-alert.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+yad \
+	--info --text 'Selamat Pagi !' --title 'Yuuko Alert' \
+	--image './yuuko_selamat_pagi_greeting-75.png' --button="OK:0" \
+	--width 200 --height 150 --text-align 'center' --buttons-layout 'center' \
+	--center
