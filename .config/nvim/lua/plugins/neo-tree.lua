2ab2bdf2b2c9bc5ba13e046203b8949849c6c69d
tfkhdyt <me@tfkhdyt.my.id>
2024-01-19 08:19:51 +0700

    feat(nvim/neo-tree): add several opts

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -2,8 +2,26 @@ return {
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		opts = {
+			sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+			open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+			filesystem = {
+				bind_to_cwd = false,
+				follow_current_file = { enabled = true },
+				use_libuv_file_watcher = true,
+			},
 			window = {
+				mappings = {
+					["<space>"] = "none",
+				},
 				position = "right",
+			},
+			default_component_configs = {
+				indent = {
+					with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+					expander_collapsed = "",
+					expander_expanded = "",
+					expander_highlight = "NeoTreeExpander",
+				},
 			},
 		},
 	},
