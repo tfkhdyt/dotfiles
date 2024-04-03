4eceb2ccabd318440d7b3a4562a3198412db9f70
tfkhdyt <tfkhdyt@proton.me>
2024-04-04 04:59:35 +0700

    feat(lsp): enable dart lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,6 +6,13 @@ return {
 				enabled = true,
 			},
 			servers = {
+				dartls = {
+					-- settings = {
+					-- 	dart = {
+					-- 		previewLsp = false,
+					-- 	},
+					},
+				},
 				gopls = {
 					keys = {
 						-- Workaround for the lack of a DAP strategy in neotest-go: https://github.com/nvim-neotest/neotest-go/issues/12
