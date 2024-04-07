19fcd0eb4f9dc6804e33e05f6d6550dfa2ec69cf
tfkhdyt <tfkhdyt@proton.me>
2024-04-07 16:23:15 +0700

    feat(nvim): setup supertab

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,10 +1,13 @@
 return {
 	{
 		"L3MON4D3/LuaSnip",
-		opts = {
-			history = true,
-			delete_check_events = "TextChanged",
-			region_check_events = "CursorMoved",
-		},
+		-- opts = {
+		-- 	history = true,
+		-- 	delete_check_events = "TextChanged",
+		-- 	region_check_events = "CursorMoved",
+		-- },
+		keys = function()
+			return {}
+		end,
 	},
 }
