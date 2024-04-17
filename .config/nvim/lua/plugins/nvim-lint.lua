2d1ba2c398f992579863f172779387cd91901fd3
tfkhdyt <tfkhdyt@proton.me>
2024-04-17 20:08:41 +0700

    fix(nvim/lint): disable document-start rule for yamllint

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -12,5 +12,25 @@ return {
 			dockerfile = { "hadolint" },
 			yaml = { "yamllint" },
 		},
+		---@type table<string,table>
+		linters = {
+			-- -- Example of using selene only when a selene.toml file is present
+			-- selene = {
+			--   -- `condition` is another LazyVim extension that allows you to
+			--   -- dynamically enable/disable linters based on the context.
+			--   condition = function(ctx)
+			--     return vim.fs.find({ "selene.toml" }, { path = ctx.filename, upward = true })[1]
+			--   end,
+			-- },
+			yamllint = {
+				args = {
+					"-d",
+					"{extends: default, rules: {document-start: false}}",
+					"--format",
+					"parsable",
+					"-",
+				},
+			},
+		},
 	},
 }
