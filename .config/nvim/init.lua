87f7ab791bcd841badedd5d7fcabf00b422d6740
tfkhdyt <tfkhdyt@proton.me>
2024-05-18 15:37:45 +0700

    neovide: add map to copy and paste with ctrl + shift

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -32,3 +32,35 @@ vim.opt.fillchars:append("eob: ")
 
 vim.filetype.add({ extension = { typ = "typst" } })
 vim.g.jsdoc_lehre_path = "/home/tfkhdyt/.local/share/pnpm/lehre"
+
+if vim.g.neovide then
+  vim.o.guifont = "JetBrainsMono NFP:h13" -- text below applies for VimScript
+  -- vim.g.neovide_cursor_vfx_mode = "railgun"
+  -- vim.g.neovide_fullscreen = true
+
+  vim.g.neovide_scale_factor = 1.0
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(1.25)
+  end)
+  vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(1 / 1.25)
+  end)
+
+  vim.keymap.set("n", "<sc-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<sc-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<sc-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<sc-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<sc-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<sc-v>", '<ESC>l"+Pli') -- Paste insert mode
+
+  vim.g.neovide_remember_window_size = false
+end
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap("", "<sc-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<sc-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<sc-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<sc-v>", "<C-R>+", { noremap = true, silent = true })
