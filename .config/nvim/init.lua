018dd9f62e1f482701e4b29503898bcf0e8dfeb6
tfkhdyt <tfkhdyt@proton.me>
2024-05-20 05:27:03 +0700

    nvim: remove some comments

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,55 +8,36 @@ vim.opt.linebreak = true
 vim.opt.colorcolumn = "80"
 vim.opt.fillchars:append("eob: ")
 
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities.offsetEncoding = "utf-8"
--- require("lspconfig").clangd.setup({
--- 	capabilities = capabilities,
--- })
-
--- rust tools
--- local rt = require("rust-tools")
---
--- rt.setup({
--- 	server = {
--- 		on_attach = function(_, bufnr)
--- 			-- Hover actions
--- 			vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
--- 			-- Code action groups
--- 			vim.keymap.set("n", "<Leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
--- 		end,
--- 	},
--- })
---
--- rt.inlay_hints.enable()
-
 vim.filetype.add({ extension = { typ = "typst" } })
 vim.g.jsdoc_lehre_path = "/home/tfkhdyt/.local/share/pnpm/lehre"
 
 if vim.g.neovide then
-  vim.o.guifont = "JetBrainsMono NFP:h13" -- text below applies for VimScript
-  -- vim.g.neovide_cursor_vfx_mode = "railgun"
-  -- vim.g.neovide_fullscreen = true
+	vim.o.guifont = "JetBrainsMono NFP:h13" -- text below applies for VimScript
+	vim.g.neovide_cursor_vfx_mode = "sonicboom"
+	-- vim.g.neovide_fullscreen = true
 
-  vim.g.neovide_scale_factor = 1.0
-  local change_scale_factor = function(delta)
-    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
-  end
-  vim.keymap.set("n", "<C-=>", function()
-    change_scale_factor(1.25)
-  end)
-  vim.keymap.set("n", "<C-->", function()
-    change_scale_factor(1 / 1.25)
-  end)
+	vim.g.neovide_scale_factor = 1.0
+	local change_scale_factor = function(delta)
+		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+	end
+	vim.keymap.set("n", "<C-=>", function()
+		change_scale_factor(1.25)
+	end)
+	vim.keymap.set("n", "<C-->", function()
+		change_scale_factor(1 / 1.25)
+	end)
 
-  vim.keymap.set("n", "<sc-s>", ":w<CR>") -- Save
-  vim.keymap.set("v", "<sc-c>", '"+y') -- Copy
-  vim.keymap.set("n", "<sc-v>", '"+P') -- Paste normal mode
-  vim.keymap.set("v", "<sc-v>", '"+P') -- Paste visual mode
-  vim.keymap.set("c", "<sc-v>", "<C-R>+") -- Paste command mode
-  vim.keymap.set("i", "<sc-v>", '<ESC>l"+Pli') -- Paste insert mode
+	vim.keymap.set("n", "<sc-s>", ":w<CR>") -- Save
+	vim.keymap.set("v", "<sc-c>", '"+y') -- Copy
+	vim.keymap.set("n", "<sc-v>", '"+P') -- Paste normal mode
+	vim.keymap.set("v", "<sc-v>", '"+P') -- Paste visual mode
+	vim.keymap.set("c", "<sc-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("i", "<sc-v>", '<ESC>l"+Pli') -- Paste insert mode
 
-  vim.g.neovide_remember_window_size = false
+	vim.g.neovide_remember_window_size = false
+
+	vim.g.neovide_text_gamma = 0.8
+	vim.g.neovide_text_contrast = 0.1
 end
 
 -- Allow clipboard copy paste in neovim
